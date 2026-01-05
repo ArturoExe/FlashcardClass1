3f54fb155f262d371e1802296486bb52a688f318
Arturo <amarturin15@gmail.com>
2026-01-05 14:40:14 -0800

    Initial Commit

---
+++ CLASS01/CLASS01App.swift
@@ -0,0 +1,17 @@
+//
+//  CLASS01App.swift
+//  CLASS01
+//
+//  Created by Arturo Martinez on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CLASS01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
